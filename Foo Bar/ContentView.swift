0e7e732a4750db2efdd629e6d845f231608b0030
Alistair White <awhite@bsd220.org>
2022-08-25 09:57:25 -0500

    Done

--- Foo Bar/ContentView.swift
+++ Foo Bar/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var num = 0
+    @State private var display = "0"
+    @State private var color = Color.black
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            color.ignoresSafeArea()
+            Text("\(display)")
+                .font(.system(size: 140))
+                .preferredColorScheme(.dark)
+                .padding()
+                .onTapGesture {
+                    num += 1
+                    if (num % 15 == 0) {
+                        display = "FOOBAR"
+                        color = .red
+                    } else if (num % 5 == 0) {
+                        display = "BAR"
+                        color = .yellow
+                    } else if (num % 3 == 0) {
+                        display = "FOO"
+                        color = .green
+                    } else {
+                        display = "\(num)"
+                        color = .black
+                    }
+                }
+                .onLongPressGesture {
+                    num = 0
+                    display = "\(num)"
+                    color = .black
+                }
+        }
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
